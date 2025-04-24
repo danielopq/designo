eff3154c193924cef1796d7b2c18fc1122de04c3
danielopq <danielopq@gmail.com>
2025-04-24 14:41:48 +0100

    BtHamburger - Switchs icon

--- src/components/layout/topNavBar/TopNavBar.tsx
+++ src/components/layout/topNavBar/TopNavBar.tsx
@@ -1,11 +1,18 @@
 import './topNavBar.scss';
 import { BtHamburger } from '../../ui';
+import { useState } from 'react';
 
 const TopNavBar: React.FC = () => {
+    const [mobileMenu, setMobileMenu] = useState<boolean>(false);
+
+    const handleClick = () => {
+        setMobileMenu(!mobileMenu);
+    }
+
     return (
         <div id="topNavBar">
             <div>
-                <BtHamburger />
+                <BtHamburger menuDeployed={mobileMenu} handleClick={handleClick} />
             </div>
         </div>
     )
--- src/components/ui/btHamburger/BtHamburger.tsx
+++ src/components/ui/btHamburger/BtHamburger.tsx
@@ -1,12 +1,13 @@
 import './btHamburger.scss';
 
 type BtHamburgerProps = {
-    menuDeployed?: boolean;
+    menuDeployed: boolean;
+    handleClick: () => void;
 }
 
-const BtHamburger: React.FC<BtHamburgerProps> = ({ menuDeployed = false }) => {
+const BtHamburger: React.FC<BtHamburgerProps> = ({ menuDeployed, handleClick }) => {
     return (
-        <button className={menuDeployed ? 'btHamburger closeIcon' : 'btHamburger hamburgerIcon'}></button>
+        <button className={menuDeployed ? 'btHamburger closeIcon' : 'btHamburger hamburgerIcon'} onClick={handleClick}></button>
     )
 }
 export default BtHamburger;
