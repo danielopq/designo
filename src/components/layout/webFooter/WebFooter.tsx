9b5c11ee598214909c3ec7ebd37167404ebe3b34
danielopq <danielopq@gmail.com>
2025-05-04 22:23:13 +0100

    WebFooter - tablet and desktop ready

--- src/components/layout/webFooter/WebFooter.tsx
+++ src/components/layout/webFooter/WebFooter.tsx
@@ -10,15 +10,17 @@ const WebFooter: React.FC = () => {
                 <NavBar navBarStyle='light' />
             </div>
             <div id="bottomWebfooter">
-                <div id="webFooterAddress">
-                    <p className='defaultTextBold'>Designo Central Office</p>
-                    <p className='defaultTextReg'>3886 Wellington Street</p>
-                    <p className='defaultTextReg'>Toronto, Ontario M9C 3J5</p>
-                </div>
-                <div id="webFooterContact">
-                    <p className='defaultTextBold'>Contact Us (Central Office)</p>
-                    <p className='defaultTextReg'>P : +1 253-863-8967</p>
-                    <p className='defaultTextReg'>M : contact@designo.co</p>
+                <div>
+                    <div id="webFooterAddress">
+                        <p className='defaultTextBold'>Designo Central Office</p>
+                        <p className='defaultTextReg'>3886 Wellington Street</p>
+                        <p className='defaultTextReg'>Toronto, Ontario M9C 3J5</p>
+                    </div>
+                    <div id="webFooterContact">
+                        <p className='defaultTextBold'>Contact Us (Central Office)</p>
+                        <p className='defaultTextReg'>P : +1 253-863-8967</p>
+                        <p className='defaultTextReg'>M : contact@designo.co</p>
+                    </div>
                 </div>
                 <div id="webFooterMedia">
                     <BtMedia imagePath='../src/assets/img/shared/icon-facebook.svg' />
