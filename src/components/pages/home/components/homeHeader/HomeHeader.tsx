a7f4bcaa588456d276c5786068aeb0c8903d0258
Daniel Martinez Duque <danielopq@gmail.com>
2025-05-14 12:21:59 +0100

    HomeHeader - tablet version done

--- src/components/pages/home/components/homeHeader/HomeHeader.tsx
+++ src/components/pages/home/components/homeHeader/HomeHeader.tsx
@@ -7,11 +7,11 @@ const HomeHeader: React.FC = () => {
         <header id="homeHeader">
             <h1>Award-winning custom designs and digital branding solutions</h1>
             <p>
-                With over 10 years in the industry, we are experienced in creating fully
-                responsive websites, app design, and engaging brand experiences.
+                With over 10 years in the industry, we are experienced in creating 
+                fully responsive websites, app design, and engaging brand experiences. 
                 Find out more about our services.
             </p>
-            <BtRegular text='GET IN TOUCH' btType='whiteBg'/>
+            <BtRegular text='LEARN MORE' btType='whiteBg' />
 
         </header>
     )
