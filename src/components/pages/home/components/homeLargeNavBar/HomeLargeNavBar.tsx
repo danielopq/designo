604d9c7ced29794ba89d6cfa25a96e0330e646da
Daniel Martinez Duque <danielopq@gmail.com>
2025-05-14 13:27:14 +0100

    BtLargeNavBar component - created

--- src/components/pages/home/components/homeLargeNavBar/HomeLargeNavBar.tsx
+++ src/components/pages/home/components/homeLargeNavBar/HomeLargeNavBar.tsx
@@ -1,8 +1,13 @@
 import './homeLargeNavBar.scss';
+import { BtLargeNavBar } from '../../../../ui';
 
 const HomeLargeNavBar:React.FC = ()=>{
     return(
-        <section id="homeLargeNavBar"></section>
+        <section id="homeLargeNavBar">
+            <BtLargeNavBar text='WEB DESIGN' gridArea='web'/>
+            <BtLargeNavBar text='APP DESIGN' gridArea='app'/>
+            <BtLargeNavBar text='GRAPHIC DESIGN' gridArea='graphic'/>
+        </section>
     )
 }
 export default HomeLargeNavBar;
