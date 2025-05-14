081d4539edfcacde80bd40416e85df21ba959522
Daniel Martinez Duque <danielopq@gmail.com>
2025-05-14 12:38:14 +0100

    HomeHeader - desktop version, comments added

--- src/components/pages/home/components/homeHeader/HomeHeader.tsx
+++ src/components/pages/home/components/homeHeader/HomeHeader.tsx
@@ -2,13 +2,18 @@ import './homeHeader.scss';
 
 import { BtRegular } from '../../../../ui';
 
+/**
+ * Renders the header displayed at the top of the Home page.
+ *
+ * @returns {React.JSX.Element} The HomeHeader component.
+ */
 const HomeHeader: React.FC = () => {
     return (
         <header id="homeHeader">
             <h1>Award-winning custom designs and digital branding solutions</h1>
             <p>
-                With over 10 years in the industry, we are experienced in creating 
-                fully responsive websites, app design, and engaging brand experiences. 
+                With over 10 years in the industry, we are experienced in creating
+                fully responsive websites, app design, and engaging brand experiences.
                 Find out more about our services.
             </p>
             <BtRegular text='LEARN MORE' btType='whiteBg' />
