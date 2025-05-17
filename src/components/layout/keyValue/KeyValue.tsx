4c1cd9ebb8fc26214f4b55a4af6fafb39865731c
Daniel Martinez Duque <danielopq@gmail.com>
2025-05-17 14:48:42 +0100

    keyValue - general layout

--- src/components/layout/keyValue/KeyValue.tsx
+++ src/components/layout/keyValue/KeyValue.tsx
@@ -1,8 +1,25 @@
 import './keyValue.scss';
 
-const KeyValue:React.FC = () => {
-    return(
-        <div className='keyValue'></div>
+type KeyValueProps = {
+
+}
+
+const KeyValue: React.FC = () => {
+    return (
+        <div className='keyValue'>
+            <div className='keyValueHero'>
+                <div></div>
+                <div></div>
+            </div>
+            <div className='KeyValueText'>
+                <p>PASSIONATE</p>
+                <p>
+                    Each project starts with an in-depth brand research to ensure
+                    we only create products that serve a purpose. We merge art,
+                    design, and technology into exciting new solutions.
+                </p>
+            </div>
+        </div>
     )
 }
 export default KeyValue;
