57adb548b73fd71a6b037ab181f22e815f1a07ec
Daniel Martinez Duque <danielopq@gmail.com>
2025-05-17 12:47:53 +0100

    btProjectsNavBar - finished

--- src/components/pages/home/components/homeProjectsNavBar/HomeProjectsNavBar.tsx
+++ src/components/pages/home/components/homeProjectsNavBar/HomeProjectsNavBar.tsx
@@ -5,6 +5,14 @@ import graphicDesignMb from '../../../../../../src/assets/img/shared/mobile/imag
 import graphicDesignTb from '../../../../../../src/assets/img/shared/tablet/image-graphic-design.jpg';
 import graphicDesignDt from '../../../../../../src/assets/img/shared/desktop/image-graphic-design.jpg';
 
+import appDesignMb from '../../../../../../src/assets/img/shared/mobile/image-app-design.jpg';
+import appDesignTb from '../../../../../../src/assets/img/shared/tablet/image-app-design.jpg';
+import appDesignDt from '../../../../../../src/assets/img/shared/desktop/image-app-design.jpg';
+
+import webDesignMb from '../../../../../../src/assets/img/shared/mobile/image-web-design.jpg';
+import webDesignTb from '../../../../../../src/assets/img/shared/tablet/image-web-design.jpg';
+import webDesignDt from '../../../../../../src/assets/img/shared/desktop/image-web-design-large.jpg';
+
 /**
  * Navigation bar on the Home page displaying different types of projects.
  * 
@@ -13,9 +21,9 @@ import graphicDesignDt from '../../../../../../src/assets/img/shared/desktop/ima
 const HomeProjectsNavBar: React.FC = () => {
     return (
         <nav id="homeProjectsNavBar">
-            <BtProjectsNavBar text='WEB DESIGN' gridArea='web' mobilePic={graphicDesignPic} tabletPic={} desktopPic={}/>
-            <BtProjectsNavBar text='APP DESIGN' gridArea='app' mobilePic={graphicDesignPic} tabletPic={} desktopPic={}/>
-            <BtProjectsNavBar text='GRAPHIC DESIGN' gridArea='graphic' mobilePic={graphicDesignPic} tabletPic={} desktopPic={}/>
+            <BtProjectsNavBar text='WEB DESIGN' gridArea='web' mobilePic={webDesignMb} tabletPic={webDesignTb} deskTopPic={webDesignDt}/>
+            <BtProjectsNavBar text='APP DESIGN' gridArea='app' mobilePic={appDesignMb} tabletPic={appDesignTb} deskTopPic={appDesignDt}/>
+            <BtProjectsNavBar text='GRAPHIC DESIGN' gridArea='graphic' mobilePic={graphicDesignMb} tabletPic={graphicDesignTb} deskTopPic={graphicDesignDt}/>
         </nav>
     )
 }
--- src/components/ui/btProjectsNavBar/BtProjectsNavBar.tsx
+++ src/components/ui/btProjectsNavBar/BtProjectsNavBar.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import './btProjectsNavBar.scss';
 
 type BtProjectsNavBarProps = {
@@ -8,12 +9,36 @@ type BtProjectsNavBarProps = {
     gridArea?: string;
 }
 
-const BtProjectsNavBar: React.FC<BtProjectsNavBarProps> = ({ text, gridArea = '', mobilePic }) => {
+const BtProjectsNavBar: React.FC<BtProjectsNavBarProps> = ({ text, gridArea = '', mobilePic, tabletPic, deskTopPic }) => {
+
+    const [backgroundImage, setBackgroundImage] = useState<string>('');
+
+    useEffect(() => {
+        updateBackgroundImage();
+        window.addEventListener('resize', updateBackgroundImage);
+        return () => {
+            window.removeEventListener('resize', updateBackgroundImage);
+        };
+    }, [mobilePic, tabletPic, deskTopPic]);
+
+    /**
+     * Updates the background image based on the current window size.
+     * Determines the screen size category (desktop, tablet, or mobile)
+     * and sets the appropriate image URL.
+     */
+    const updateBackgroundImage = () => {
+        const isDesktop = window.innerWidth >= 1024;
+        const isTablet = window.innerWidth >= 768 && window.innerWidth < 1024;
+        setBackgroundImage(isDesktop ? deskTopPic : isTablet ? tabletPic : mobilePic);
+    };
+
     return (
-        <div className='btProjectsNavBar' style={{ gridArea: gridArea, backgroundImage: `url(${mobilePic})` }}>
-            <div>
-                <p>{text}</p>
-                <div>VIEW PROJECTS</div>
+        <div className='btProjectsNavBar' style={{ gridArea: gridArea, backgroundImage: `url(${backgroundImage})` }}>
+            <div className='btPNBcontent'>
+                <div className='btPNBtext'>
+                    <p>{text}</p>
+                    <div>VIEW PROJECTS</div>
+                </div>
             </div>
         </div>
     )
