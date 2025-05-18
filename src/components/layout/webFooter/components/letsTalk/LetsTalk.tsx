9af9dcce93c725a76d1cd07f4f0d7916a3769bf5
Daniel Martinez Duque <danielopq@gmail.com>
2025-05-18 11:17:44 +0100

    LetsTalk - div added

--- src/components/layout/webFooter/components/letsTalk/LetsTalk.tsx
+++ src/components/layout/webFooter/components/letsTalk/LetsTalk.tsx
@@ -5,11 +5,13 @@ import { BtRegular } from '../../../../ui';
 const LetsTalk: React.FC = () => {
     return (
         <section id="letsTalk">
-            <p>Let’s talk about your project</p>
-            <p>
-                Ready to take it to the next level? Contact us today and find out
-                how our expertise can help your business grow.
-            </p>
+            <div>
+                <p>Let’s talk about your project</p>
+                <p>
+                    Ready to take it to the next level? Contact us today and find out
+                    how our expertise can help your business grow.
+                </p>
+            </div>
             <BtRegular text='GET IN TOUCH' btType='whiteBg' />
         </section>
     )
