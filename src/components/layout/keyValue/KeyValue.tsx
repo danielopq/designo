ff84998741cf20af156b436f843d156f5b249d34
Daniel Martinez Duque <danielopq@gmail.com>
2025-05-18 08:57:39 +0100

    Values and KeyValues components - ready

--- src/components/layout/keyValue/KeyValue.tsx
+++ src/components/layout/keyValue/KeyValue.tsx
@@ -1,23 +1,28 @@
 import './keyValue.scss';
 
 type KeyValueProps = {
-
+    keyPicPath: string;
+    keyTitle: string;
+    keyText: string;
 }
 
-const KeyValue: React.FC = () => {
+/**
+ * Component that renders a key company value with an image,
+ * a title, and a descriptive text.
+ * 
+ * @param {Object} props - Component properties.
+ * @param {string} props.keyPicPath - Path to the image used as the background.
+ * @param {string} props.keyTitle - Title of the key value.
+ * @param {string} props.keyText - Description of the key value.
+ * @returns {JSX.Element} The rendered KeyValue component.
+ */
+const KeyValue: React.FC<KeyValueProps> = ({ keyPicPath, keyTitle, keyText }) => {
     return (
         <div className='keyValue'>
-            <div className='keyValueHero'>
-                <div></div>
-                <div></div>
-            </div>
+            <div className='keyValueHero' style={{ backgroundImage: `url(${keyPicPath})` }}></div>
             <div className='KeyValueText'>
-                <p>PASSIONATE</p>
-                <p>
-                    Each project starts with an in-depth brand research to ensure
-                    we only create products that serve a purpose. We merge art,
-                    design, and technology into exciting new solutions.
-                </p>
+                <p>{keyTitle.toLocaleUpperCase()}</p>
+                <p>{keyText}</p>
             </div>
         </div>
     )
--- src/components/pages/home/components/values/Values.tsx
+++ src/components/pages/home/components/values/Values.tsx
@@ -1,13 +1,39 @@
 import './values.scss';
-
 import { KeyValue } from '../../../../layout';
 
+import passionatePic from '../../../../../assets/img/home/illustration-passionate.svg';
+import resourcefulPic from '../../../../../assets/img/home/illustration-resourceful.svg';
+import friendlyPic from '../../../../../assets/img/home/illustration-friendly.svg';
+
+/**
+ * Renders the company values included on the home page.
+ * 
+ * @returns {JSX.Element} - The rendered Values component. 
+ */
 const Values: React.FC = () => {
     return (
         <section id="values">
-            <KeyValue />
-            <KeyValue />
-            <KeyValue />
+            <KeyValue
+                keyPicPath={passionatePic}
+                keyTitle='PASSIONATE'
+                keyText='Each project starts with an in-depth brand research to ensure we only 
+            create products that serve a purpose. We merge art, design, and technology 
+            into exciting new solutions.'
+            />
+            <KeyValue
+                keyPicPath={resourcefulPic}
+                keyTitle='RESOURCEFUL'
+                keyText='Everything that we do has a strategic purpose. We use an agile approach 
+                in all of our projects and value customer collaboration. It guarantees superior
+                 results that fulfill our clients’ needs.'
+            />
+            <KeyValue
+                keyPicPath={friendlyPic}
+                keyTitle='FRIENDLY'
+                keyText=' We are a group of enthusiastic folks who know how to put people first. 
+                Our success depends on our customers, and we strive to give them the best 
+                experience a company can provide.'
+            />
         </section>
     )
 }
