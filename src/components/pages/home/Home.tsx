cccb5685f92b332e8778b0a022fc9294429c1834
Daniel Martinez Duque <danielopq@gmail.com>
2025-05-18 10:00:27 +0100

    Home - various adjusments

--- src/components/pages/home/Home.tsx
+++ src/components/pages/home/Home.tsx
@@ -2,6 +2,11 @@ import './home.scss';
 
 import { HomeHeader, HomeProjectsNavBar, Values } from './components';
 
+/**
+ * Contains the home page.
+ * 
+ * @returns {React.JSX.Element} - The rendered Home component. 
+ */
 const Home: React.FC = () => {
     return (
         <main id="home">
