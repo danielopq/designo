7d8321ff36e5a9886d2ab091b699d2cdc587d6d8
Daniel Martinez Duque <danielopq@gmail.com>
2025-05-24 14:08:10 +0100

    HomeProjectsNavBar - images import modified

--- src/components/pages/home/components/homeProjectsNavBar/HomeProjectsNavBar.tsx
+++ src/components/pages/home/components/homeProjectsNavBar/HomeProjectsNavBar.tsx
@@ -1,17 +1,8 @@
 import './homeProjectsNavBar.scss';
 import { BtProjectsNavBar } from '../../../../ui';
 
-import graphicDesignMb from '../../../../../../src/assets/img/shared/mobile/image-graphic-design.jpg';
-import graphicDesignTb from '../../../../../../src/assets/img/shared/tablet/image-graphic-design.jpg';
-import graphicDesignDt from '../../../../../../src/assets/img/shared/desktop/image-graphic-design.jpg';
+import { appMobilePic, appTabletPic, appDesktopPic, graphicMobilePic, graphicTabletPic, graphicDesktopPic, webMobilePic, webTabletPic, webDesktopLargePic } from '../../../../../../src/assets/img/shared';
 
-import appDesignMb from '../../../../../../src/assets/img/shared/mobile/image-app-design.jpg';
-import appDesignTb from '../../../../../../src/assets/img/shared/tablet/image-app-design.jpg';
-import appDesignDt from '../../../../../../src/assets/img/shared/desktop/image-app-design.jpg';
-
-import webDesignMb from '../../../../../../src/assets/img/shared/mobile/image-web-design.jpg';
-import webDesignTb from '../../../../../../src/assets/img/shared/tablet/image-web-design.jpg';
-import webDesignDt from '../../../../../../src/assets/img/shared/desktop/image-web-design-large.jpg';
 
 /**
  * Navigation bar on the Home page displaying different types of projects.
@@ -21,9 +12,9 @@ import webDesignDt from '../../../../../../src/assets/img/shared/desktop/image-w
 const HomeProjectsNavBar: React.FC = () => {
     return (
         <nav id="homeProjectsNavBar">
-            <BtProjectsNavBar text='WEB DESIGN' gridArea='web' mobilePic={webDesignMb} tabletPic={webDesignTb} deskTopPic={webDesignDt}/>
-            <BtProjectsNavBar text='APP DESIGN' gridArea='app' mobilePic={appDesignMb} tabletPic={appDesignTb} deskTopPic={appDesignDt}/>
-            <BtProjectsNavBar text='GRAPHIC DESIGN' gridArea='graphic' mobilePic={graphicDesignMb} tabletPic={graphicDesignTb} deskTopPic={graphicDesignDt}/>
+            <BtProjectsNavBar text='WEB DESIGN' gridArea='web' mobilePic={webMobilePic} tabletPic={webTabletPic} deskTopPic={webDesktopLargePic} />
+            <BtProjectsNavBar text='APP DESIGN' gridArea='app' mobilePic={appMobilePic} tabletPic={appTabletPic} deskTopPic={appDesktopPic} />
+            <BtProjectsNavBar text='GRAPHIC DESIGN' gridArea='graphic' mobilePic={graphicMobilePic} tabletPic={graphicTabletPic} deskTopPic={graphicDesktopPic} />
         </nav>
     )
 }
