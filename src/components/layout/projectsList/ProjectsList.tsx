6a95fceac48bf7b1a18244dac28aa42e6ee8164d
Daniel Martinez Duque <danielopq@gmail.com>
2025-05-24 14:55:45 +0100

    ProjectsList - comments added

--- src/components/layout/projectsList/ProjectsList.tsx
+++ src/components/layout/projectsList/ProjectsList.tsx
@@ -5,6 +5,13 @@ type ProjectsListProps = {
     children: ReactNode;
 }
 
+/**
+ * Renders a container for displaying a list of company projects.
+ *
+ * @param {Object} param0 - Component props.
+ * @param {ReactNode} param0.children - The child elements representing project items.
+ * @returns {JSX.Element} -- The ProjectsList component. A section element containing the project items.
+ */
 const ProjectsList: React.FC<ProjectsListProps> = ({children}) => {
     return (
         <section className='projectsList'>
