d870a520b47735e22b915cd341e8f430a68d0dfb
Daniel Martinez Duque <danielopq@gmail.com>
2025-05-25 11:48:13 +0100

    ProjectView - comments added

--- src/components/layout/projectView/ProjectView.tsx
+++ src/components/layout/projectView/ProjectView.tsx
@@ -1,11 +1,20 @@
 import './projectView.scss';
 
 type ProjectViewProps = {
-    pic: string;
-    title: string;
-    description: string;
+    pic: string;          // URL of the project's image
+    title: string;        // Title of the project
+    description: string;  // Short description of the project
 }
 
+/**
+ * ProjectView component displays a project card with an image, title, and description.
+ *
+ * @param {Object} props - Component props
+ * @param {string} props.pic - URL of the project's image
+ * @param {string} props.title - Title of the project
+ * @param {string} props.description - Description of the project
+ * @returns {JSX.Element} Rendered project card component
+ */
 const ProjectView: React.FC<ProjectViewProps> = ({ pic, title, description }) => {
     return (
         <article className='projectView'>
