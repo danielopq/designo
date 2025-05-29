c5a4aad57241fa54611fade4ebdd27fbe7a04bba
danielopq <danielopq@gmail.com>
2025-05-29 14:40:46 +0100

    Talent - tablet version done

--- src/components/pages/about/component/talent/Talent.tsx
+++ src/components/pages/about/component/talent/Talent.tsx
@@ -10,7 +10,7 @@ const Talent: React.FC = () => {
                     We are a crew of strategists, problem-solvers, and technologists. Every design
                     is thoughtfully crafted from concept to launch, ensuring success in its given
                     market. We are constantly updating our skills in a myriad of platforms.
-
+                    <br /><br />
                     Our team is multi-disciplinary and we are not merely interested in form —
                     content and meaning are just as important. We give great importance to craftsmanship,
                     service, and prompt delivery. Clients have always been impressed with our high-quality
