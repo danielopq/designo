d281183493d912b55b806ca25fb709abd613e8bc
danielopq <danielopq@gmail.com>
2025-06-09 17:25:37 +0100

    offices.json created, LocationDetails - done

--- src/components/pages/locations/Locations.tsx
+++ src/components/pages/locations/Locations.tsx
@@ -1,13 +1,31 @@
 import './locations.scss';
 
+import data from '../../../assets/data/offices.json';
 import LocationDetails from './components/LocationDetails';
+
+type details = {
+    country: string;
+    branch: string;
+    firstLineAdd: string;
+    secondLineAdd: string;
+    phone: string;
+    email: string;
+}
 
 const Locations: React.FC = () => {
     return (
         <main id="locations">
-            <LocationDetails />
-            <LocationDetails />
-            <LocationDetails />
+            {data.offices.map((office: details, index: number) => (
+                <LocationDetails
+                    key={index}
+                    country={office.country}
+                    branch={office.branch}
+                    firstLineAdd={office.firstLineAdd}
+                    secondLineAdd={office.secondLineAdd}
+                    phone={office.phone}
+                    email={office.email}
+                />
+            ))}
         </main>
     )
 }
--- src/components/pages/locations/components/LocationDetails.tsx
+++ src/components/pages/locations/components/LocationDetails.tsx
@@ -1,21 +1,30 @@
 import './locationDetails.scss';
 
-const LocationDetails = () => {
+type LocationDetailsProps = {
+    country: string;
+    branch: string;
+    firstLineAdd: string;
+    secondLineAdd: string;
+    phone: string;
+    email: string;
+}
+
+const LocationDetails:React.FC<LocationDetailsProps> = ({country,branch,firstLineAdd,secondLineAdd,phone,email}) => {
     return (
         <div className='locationDetails'>
             <div className='locationMap'></div>
             <div className='locationOffice'>
-                <h1 className='LocationCountry'>Canada</h1>
+                <h1 className='LocationCountry'>{country}</h1>
                 <div className='detailsCont'>
                     <div>
-                        <p className='detailsHeader'>Designo Central Office</p>
-                        <p className='detailsLine'>3886 Wellington Street</p>
-                        <p className='detailsLine'>Toronto, Ontario M9C 3J5</p>
+                        <p className='detailsHeader'>{branch}</p>
+                        <p className='detailsLine'>{firstLineAdd}</p>
+                        <p className='detailsLine'>{secondLineAdd}</p>
                     </div>
                     <div>
                         <p className='detailsHeader'>Contact</p>
-                        <p className='detailsLine'>P : +1 253-863-8967</p>
-                        <p className='detailsLine'>M : contact@designo.co</p>
+                        <p className='detailsLine'>P : {phone}</p>
+                        <p className='detailsLine'>M : {email}</p>
                     </div>
                 </div>
             </div>
