cb381cf5387ba84d9575a7c064b2657b0ce092ce
Daniel Martinez Duque <danielopq@gmail.com>
2025-06-10 15:44:46 +0100

    LetsTalk - navigation active, it hides when locatio.pathname = /contact

--- src/components/layout/webFooter/components/letsTalk/LetsTalk.tsx
+++ src/components/layout/webFooter/components/letsTalk/LetsTalk.tsx
@@ -1,12 +1,14 @@
 import './letsTalk.scss';
 
+import { useWebNavigation } from '../../../../../utils/webNavigation';
 import { BtRegular } from '../../../../ui';
 import { useLocation } from 'react-router-dom';
 
 const LetsTalk: React.FC = () => {
-     const location = useLocation();
+    const location = useLocation();
+    const { navigateTo } = useWebNavigation();
     return (
-        <section id="letsTalk">
+        <section id="letsTalk" style={{display: location.pathname === '/contact' ? 'none' : 'flex'}}>
             <div>
                 <p>Let’s talk about your project</p>
                 <p>
@@ -14,7 +16,7 @@ const LetsTalk: React.FC = () => {
                     how our expertise can help your business grow.
                 </p>
             </div>
-            <BtRegular text='GET IN TOUCH' btType='whiteBg' />
+            <BtRegular text='GET IN TOUCH' btType='whiteBg' handleClick={() => navigateTo('/contact')} />
         </section>
     )
 }
