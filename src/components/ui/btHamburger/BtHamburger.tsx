4bf49689e489d3d4915c7af935e9eb12c506adb9
Daniel Martinez Duque <danielopq@gmail.com>
2025-06-19 14:42:44 +0100

    BtHamburger - comments added

--- src/components/ui/btHamburger/BtHamburger.tsx
+++ src/components/ui/btHamburger/BtHamburger.tsx
@@ -5,6 +5,14 @@ type BtHamburgerProps = {
     handleClick: () => void;
 }
 
+/**
+ * Renders the hamburger menu button for the mobile version of the website.
+ * 
+ * @param {boolean} menuDeployed - Indicates whether the mobile menu is currently open.
+ * @param {() => void} handleClick - Function to toggle the menu's visibility.
+ * 
+ * @returns {JSX.Element} The hamburger button component.
+ */
 const BtHamburger: React.FC<BtHamburgerProps> = ({ menuDeployed, handleClick }) => {
     return (
         <button className={menuDeployed ? 'btHamburger closeIcon' : 'btHamburger hamburgerIcon'} onClick={handleClick}></button>
